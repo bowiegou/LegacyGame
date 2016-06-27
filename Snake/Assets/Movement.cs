c1a8815982453c6c4a481db3a920fd9eed4ac2a5
bowiegou <i@bowiegou.com>
2016-06-27 17:50:02 +0800

    movement: border case

--- Snake/Assets/Movement.cs
+++ Snake/Assets/Movement.cs
@@ -7,8 +7,9 @@ using System.Collections.Generic;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour {
 
-    public static Queue<MoveMentMode> MoveMentQueue = new Queue<MoveMentMode>();
-
+    public static Queue<MoveMentMode> MovementQueue = new Queue<MoveMentMode>();
+    public static Camera MainCam = Camera.main;
+    public static float SpeedMutiplier = 10f;
 
     public enum MoveMentMode {
         Up,
@@ -27,6 +28,7 @@ public class Movement : MonoBehaviour {
     private bool _isBody = true;
     private bool _isTail = true;
     private Rigidbody2D _rigidbody;
+    private MoveMentMode _lastMode;
 
 	// Use this for initialization
 	void Start () {
@@ -36,41 +38,65 @@ public class Movement : MonoBehaviour {
 	}
 
     void SetNextActionMode(MoveMentMode mode) {
-        lock (MoveMentQueue) {
-            MoveMentQueue.Enqueue(mode);
+        lock (MovementQueue) {
+            MovementQueue.Enqueue(mode);
         }
+        _lastMode = mode;
 
     }
 
     void NextAction() {
         MoveMentMode next = MoveMentMode.Default;
-        lock (MoveMentQueue) {
-            next = MoveMentQueue.Peek();
+        lock (MovementQueue) {
+            if(MovementQueue.Count <= 0) return;
+            next = MovementQueue.Peek();
         }
+        Vector3 thisPosition = this.transform.position;
+        
 
         switch (next) {
+
                 case MoveMentMode.Right: {
-                    this._rigidbody.velocity = new Vector2(1f, 0.0f);
+                    this._rigidbody.velocity = new Vector2(1f * SpeedMutiplier, 0.0f);
                     break;
                 }
                 case MoveMentMode.Left: {
-                    this._rigidbody.velocity = new Vector2(-1f, 0.0f);
+                    this._rigidbody.velocity = new Vector2(-1f * SpeedMutiplier, 0.0f);
                     break;
                 }
                 case MoveMentMode.Up: {
-                    this._rigidbody.velocity = new Vector2(0.0f, 1f);
+                    this._rigidbody.velocity = new Vector2(0.0f, 1f * SpeedMutiplier);
                     break;
                 }
                 case MoveMentMode.Down: {
-                    this._rigidbody.velocity = new Vector2(0.0f, -1f);
+                    this._rigidbody.velocity = new Vector2(0.0f, -1f * SpeedMutiplier);
                     break;
                 }
+                
+
+                case MoveMentMode.RightBorder: {
+                    this.transform.position = new Vector3(MainCam.ViewportToWorldPoint(new Vector3(1f,0f,0f)).x , thisPosition.y, 0.0f );
+                    break;
+                }
+                case MoveMentMode.LeftBorder: {
+                    this.transform.position = new Vector3(MainCam.ViewportToWorldPoint(new Vector3(0f, 0f, 0f)).x, thisPosition.y, 0.0f);
+                    break;
+                }
+                case MoveMentMode.UpBorder: {
+                    this.transform.position = new Vector3(thisPosition.x,(MainCam.ViewportToWorldPoint(new Vector3(0f, 1f, 0f)).y));
+                    break;
+                }
+                case MoveMentMode.DownBorder: {
+                    this.transform.position = new Vector3(thisPosition.x, (MainCam.ViewportToWorldPoint(new Vector3(0f, 0f, 0f)).y));
+                    break;
+                }
+                
         }
 
 
         if (_isTail) {       
-            lock (MoveMentQueue) {
-                MoveMentQueue.Dequeue();
+            lock (MovementQueue) {
+                MovementQueue.Dequeue();
             }
         }
 
@@ -89,25 +115,25 @@ public class Movement : MonoBehaviour {
 //	        return;
 //	    }
 
-        Camera mainCam = Camera.main;
 	    Vector3 thisPosition = this.transform.position;
-	    Vector3 viewPortPostion = mainCam.WorldToViewportPoint(thisPosition);
-        Debug.Log(viewPortPostion);
+	    Vector3 viewPortPostion = MainCam.WorldToViewportPoint(thisPosition);
+        //Debug.Log(viewPortPostion);
 	   // Vector3 position = this.transform.position;
-	    if (viewPortPostion.x >= 1) {
-	        //reach right border of camera
-	        SetNextActionMode(MoveMentMode.LeftBorder);
-            return;
-	    } else if (viewPortPostion.x <= 0) {
-	        SetNextActionMode(MoveMentMode.RightBorder);
-            return;
-	    } else if (viewPortPostion.y <= 0) {
-	        SetNextActionMode(MoveMentMode.UpBorder);
-            return;
-	    } else if (viewPortPostion.y >= 1) {
-	        SetNextActionMode(MoveMentMode.DownBorder);
-            return;
-	    }
+	   // if (_lastMode != MoveMentMode.DownBorder && _lastMode != MoveMentMode.RightBorder && _lastMode != MoveMentMode.LeftBorder && _lastMode != MoveMentMode.UpBorder) {
+	        if (viewPortPostion.x >= 1) {
+	            //reach right border of camera
+	            SetNextActionMode(MoveMentMode.LeftBorder);
+	        }
+	        else if (viewPortPostion.x <= 0) {
+	            SetNextActionMode(MoveMentMode.RightBorder);
+	        }
+	        else if (viewPortPostion.y <= 0) {
+	            SetNextActionMode(MoveMentMode.UpBorder);
+	        }
+	        else if (viewPortPostion.y >= 1) {
+	            SetNextActionMode(MoveMentMode.DownBorder);
+	        }
+	    //}
 
 	    if (Input.GetKey("up")) {
 	        SetNextActionMode(MoveMentMode.Up);
