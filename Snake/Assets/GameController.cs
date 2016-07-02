659645f08e1bad54ccee06039aad6fbd05c2047b
bowiegou <i@bowiegou.com>
2016-07-02 23:08:14 +0800

    added score logic

---
+++ Snake/Assets/GameController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController {
+	public Score GameScore;
+
+
+
+}
+
+public struct Score {
+	public int FoodAte;
+	public int TotalScore;
+}
