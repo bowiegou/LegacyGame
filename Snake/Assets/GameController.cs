7384348cc7cdec5c195bfac6b07b7f07991ce33d
bowiegou <i@bowiegou.com>
2016-07-03 23:35:56 +0800

    Snake: some update to score

--- Snake/Assets/GameController.cs
+++ Snake/Assets/GameController.cs
@@ -4,11 +4,35 @@ using System.Collections;
 public class GameController {
 	public Score GameScore;
 
+	public GameController() {
+		this.GameScore = new Score ();
+	}
 
+
+
+	void OnFoodAte() {
+		this.GameScore.FoodAte++;
+		//TODO: fix total score
+		this.GameScore.TotalScore++;
+
+	}
 
 }
 
 public struct Score {
 	public int FoodAte;
 	public int TotalScore;
+
+	public Score (int foodAte = 0, int totalScore = 0)
+	{
+		this.FoodAte = foodAte;
+		this.TotalScore = totalScore;
+	}
+
+	public int getTotalScore() {
+		return this.TotalScore;
+	}
+	
+
+
 }
