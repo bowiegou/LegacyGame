da779bba6da099283ab3e47271edeb98317e9121
bowiegou <i@bowiegou.com>
2016-07-05 00:08:28 +0800

    update score logic

--- Snake/Assets/GameController.cs
+++ Snake/Assets/GameController.cs
@@ -3,17 +3,16 @@ using System.Collections;
 
 public class GameController {
 	public Score GameScore;
+	public int ScoreMutiplier = 10;
 
 	public GameController() {
-		this.GameScore = new Score ();
+		this.GameScore = new Score (10);
 	}
 
 
 
 	void OnFoodAte() {
-		this.GameScore.FoodAte++;
-		//TODO: fix total score
-		this.GameScore.TotalScore++;
+		GameScore.updateScore ();
 
 	}
 
@@ -22,17 +21,28 @@ public class GameController {
 public struct Score {
 	public int FoodAte;
 	public int TotalScore;
+	int scoreMutiplier;
 
-	public Score (int foodAte = 0, int totalScore = 0)
+
+	public Score (int scoreMutiplier ,int foodAte = 0, int totalScore = 0)
 	{
 		this.FoodAte = foodAte;
 		this.TotalScore = totalScore;
+		this.scoreMutiplier = scoreMutiplier;
 	}
 
 	public int getTotalScore() {
 		return this.TotalScore;
 	}
-	
+
+	public int updateScore() {
+		updateScore (++this.FoodAte);
+	}
+
+	public int updateScore(int FoodAte) {
+		this.FoodAte = FoodAte;
+		this.TotalScore = FoodAte * scoreMutiplier;
+	}
 
 
 }
